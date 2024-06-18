09ac7c7ad592c2fa229ea807b6f71c5c9d89a7d9
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-18 14:51:23 +0300

    feat: create xml util class for converting dtos to string

---
+++ gepg/src/main/java/com/watabelabs/gepg/utils/XmlUtil.java
@@ -0,0 +1,29 @@
+package com.watabelabs.gepg.utils;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+/**
+ * Utility class for converting Java objects to XML strings using JAXB.
+ */
+public class XmlUtil {
+
+    /**
+     * Converts a JAXB-annotated object to an XML string.
+     *
+     * @param object the JAXB-annotated object to convert
+     * @return the XML string representation of the object
+     * @throws Exception if an error occurs during XML conversion
+     */
+    public static String convertToXmlString(Object object) throws Exception {
+        JAXBContext context = JAXBContext.newInstance(object.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        StringWriter sw = new StringWriter();
+        marshaller.marshal(object, sw);
+        return sw.toString();
+    }
+}
