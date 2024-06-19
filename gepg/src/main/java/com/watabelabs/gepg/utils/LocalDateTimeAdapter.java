8e92dc3d7eeaaa3352b89675fdf9a579f49444d3
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-19 08:28:52 +0300

    feat: create a local datetime adapter

---
+++ gepg/src/main/java/com/watabelabs/gepg/utils/LocalDateTimeAdapter.java
@@ -0,0 +1,20 @@
+package com.watabelabs.gepg.utils;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
+    private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+    @Override
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(v, DATE_FORMAT);
+    }
+
+    @Override
+    public String marshal(LocalDateTime v) throws Exception {
+        return v.format(DATE_FORMAT);
+    }
+}
+
