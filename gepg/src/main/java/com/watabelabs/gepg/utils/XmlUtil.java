90489a16243c91778f64073917029631757dee01
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-19 06:23:40 +0300

    feat: add method in the XmlUtil class to convert a dto to xml string without adding xml declaration

--- gepg/src/main/java/com/watabelabs/gepg/utils/XmlUtil.java
+++ gepg/src/main/java/com/watabelabs/gepg/utils/XmlUtil.java
@@ -26,4 +26,23 @@ public class XmlUtil {
         marshaller.marshal(object, sw);
         return sw.toString();
     }
+
+    /**
+     * Converts a JAXB-annotated object to an XML string without the XML declaration.
+     *
+     * @param object the JAXB-annotated object to convert
+     * @return the XML string representation of the object without the XML declaration
+     * @throws Exception if an error occurs during XML conversion
+     */
+    public static String convertToXmlStringWithoutDeclaration(Object object) throws Exception {
+        JAXBContext context = JAXBContext.newInstance(object.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+
+        StringWriter sw = new StringWriter();
+        marshaller.marshal(object, sw);
+        return sw.toString();
+    }
 }
+
