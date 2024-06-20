6bee7f1d93d5fecf0767b997e34efc1bc2d8146c
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-20 07:12:04 +0300

    feat: add method to unwrap xml string into a pojo

---
+++ gepg/src/main/java/com/watabelabs/gepg/utils/CDataAdapter.java
@@ -0,0 +1,16 @@
+package com.watabelabs.gepg.utils;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class CDataAdapter extends XmlAdapter<String, String> {
+
+    @Override
+    public String marshal(String v) throws Exception {
+        return "<![CDATA[" + v + "]]>";
+    }
+
+    @Override
+    public String unmarshal(String v) throws Exception {
+        return v;
+    }
+}
--- gepg/src/main/java/com/watabelabs/gepg/utils/MessageUtil.java
+++ gepg/src/main/java/com/watabelabs/gepg/utils/MessageUtil.java
@@ -1,20 +1,24 @@
 package com.watabelabs.gepg.utils;
 
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.security.PrivateKey;
+import java.util.Collections;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.ValidationException;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * The MessageUtil class provides functionality to sign messages using a digital
- * signature.
- * It wraps the message and the signature into an XML envelope.
+ * signature. It wraps the message and the signature into an XML envelope.
  *
  * <p>
  * The digital signature is generated using a private key loaded from a PKCS#12
@@ -36,7 +40,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * String message = "<gepgBillSubReq>...</gepgBillSubReq>";
  *
  * // Sign the message
- * String signedMessage = messageUtil.sign(message);
+ * String signedMessage = messageUtil.sign(message, GepgBillSubReq.class);
  * System.out.println(signedMessage);
  * }
  * </pre>
@@ -72,12 +76,12 @@ public class MessageUtil {
      * Signs the provided message using the private key and wraps it in an XML
      * envelope.
      *
-     * @param message the message to be signed
+     * @param message      the message to be signed
+     * @param contentClass the class of the content to be wrapped
      * @return the signed message wrapped in an XML envelope
      * @throws Exception if an error occurs during signing or XML conversion
      */
-
-    public String sign(String message) throws Exception {
+    public <T> String sign(String message, Class<T> contentClass) throws Exception {
         if (message == null || message.isEmpty()) {
             throw new ValidationException("Message cannot be null or empty");
         }
@@ -85,23 +89,40 @@ public class MessageUtil {
         PrivateKey privateKey = DigitalSignatureUtil.loadPrivateKey(keystorePath, keystorePassword, keyAlias);
         String digitalSignature = DigitalSignatureUtil.signData(message, privateKey);
 
-        Envelope envelope = new Envelope();
-        envelope.setMessage(message);
-        envelope.setDigitalSignature(digitalSignature);
+        T content = parseContent(message, contentClass);
 
-        return convertToXmlString(envelope);
+        Envelope<T> envelope = new Envelope<>();
+        envelope.setContent(Collections.singletonList(content));
+        envelope.setGepgSignature(digitalSignature);
+
+        return convertToXmlString(envelope, contentClass);
+    }
+
+    /**
+     * Parses the content from the provided message.
+     *
+     * @param message      the message containing the content
+     * @param contentClass the class of the content to be parsed
+     * @return the parsed content
+     * @throws Exception if an error occurs during parsing
+     */
+    private <T> T parseContent(String message, Class<T> contentClass) throws Exception {
+        JAXBContext context = JAXBContext.newInstance(contentClass);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        return contentClass.cast(unmarshaller.unmarshal(new StringReader(message)));
     }
 
     /**
      * Converts the envelope object to an XML string.
      *
-     * @param envelope the envelope object containing the message and the digital
-     *                 signature
+     * @param envelope     the envelope object containing the content and the
+     *                     digital signature
+     * @param contentClass the class of the content to be wrapped
      * @return the XML string representation of the envelope
      * @throws Exception if an error occurs during XML conversion
      */
-    private static String convertToXmlString(Envelope envelope) throws Exception {
-        JAXBContext context = JAXBContext.newInstance(Envelope.class);
+    private <T> String convertToXmlString(Envelope<T> envelope, Class<T> contentClass) throws Exception {
+        JAXBContext context = JAXBContext.newInstance(Envelope.class, contentClass);
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
@@ -111,55 +132,54 @@ public class MessageUtil {
     }
 
     /**
-     * The Envelope class wraps the message and the digital signature into an XML
+     * Unwraps the provided XML string from the envelope and converts it to the
+     * specified POJO class.
+     *
+     * @param xmlString    the XML string containing the envelope
+     * @param contentClass the class of the content to be extracted
+     * @return the content as a POJO
+     * @throws Exception if an error occurs during XML conversion
+     */
+    public <T> T unwrapAndConvertToPojo(String xmlString, Class<T> contentClass) throws Exception {
+        // Unmarshal the Envelope
+        JAXBContext context = JAXBContext.newInstance(Envelope.class, contentClass);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Envelope<T> envelope = (Envelope<T>) unmarshaller.unmarshal(new StringReader(xmlString));
+
+        ct the content and return the first item (assuming there's only one)
+        return envelope.getContent().get(0);
+    }
+
+    /**
+     * The Envelope class wraps any content and the digital signature into an XML
      * structure.
      */
     @XmlRootElement(name = "Gepg")
     @XmlAccessorType(XmlAccessType.FIELD)
-    public static class Envelope {
+    public static class Envelope<T> {
 
-        @XmlElement(name = "message")
-        private String message;
+        @XmlAnyElement(lax = true)
+        private List<T> content;
 
-        @XmlElement(name = "gepgSignature")
-        private String digitalSignature;
+        @XmlElement(name = "gepgSignature", required = true)
+        private String gepgSignature;
 
         // Getters and setters
 
-        /**
-         * Retrieves the message.
-         *
-         * @return the message
-         */
-        public String getMessage() {
-            return message;
+        public List<T> getContent() {
+            return content;
         }
 
-        /**
-         * Sets the message.
-         *
-         * @param message the message to set
-         */
-        public void setMessage(String message) {
-            this.message = message;
+        public void setContent(List<T> content) {
+            this.content = content;
         }
 
-        /**
-         * Retrieves the digital signature.
-         *
-         * @return the digital signature
-         */
-        public String getDigitalSignature() {
-            return digitalSignature;
+        public String getGepgSignature() {
+            return gepgSignature;
         }
 
-        /**
-         * Sets the digital signature.
-         *
-         * @param digitalSignature the digital signature to set
-         */
-        public void setDigitalSignature(String digitalSignature) {
-            this.digitalSignature = digitalSignature;
+        public void setGepgSignature(String gepgSignature) {
+            this.gepgSignature = gepgSignature;
         }
     }
 }
