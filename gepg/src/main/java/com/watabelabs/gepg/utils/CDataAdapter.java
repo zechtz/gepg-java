cb88ad75918b96c9a676c7fbc90ac64434e508c9
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-22 13:24:37 +0300

    feat: create date adapter

--- gepg/src/main/java/com/watabelabs/gepg/utils/CDataAdapter.java
+++ gepg/src/main/java/com/watabelabs/gepg/utils/CDataAdapter.java
@@ -1,6 +1,6 @@
 package com.watabelabs.gepg.utils;
 
-import javax.xml.bind.annotation.adapters.XmlAdapter;
+import jakarta.xml.bind.annotation.adapters.XmlAdapter;
 
 public class CDataAdapter extends XmlAdapter<String, String> {
 
