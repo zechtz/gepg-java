003a72218e1c20b5d1e6436771d61132fa2a8378
Zachariah Ngonyani <zachariahn@gmail.com>
2024-06-27 09:04:07 +0300

    feat: refactor PmtSpInfo test to only use GepgApiClient

--- gepg/src/test/java/com/watabelabs/gepg/mappers/payment/GepgPmtSpInfoMapperTest.java
+++ gepg/src/test/java/com/watabelabs/gepg/mappers/payment/GepgPmtSpInfoMapperTest.java
@@ -11,20 +11,17 @@ import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import com.watabelabs.gepg.utils.MessageUtil;
-import com.watabelabs.gepg.utils.XmlUtil;
-
+import com.watabelabs.gepg.GepgApiClient;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class GepgPmtSpInfoMapperTest {
 
-    private static String keystorePath;
-    private static String keystorePassword;
-    private static String keyAlias;
+    private static GepgApiClient gepgApiClient;
 
     @BeforeAll
     public static void setup() {
+        gepgApiClient = new GepgApiClient();
         try {
             // Load the keystore file from the classpath
             InputStream resourceStream = GepgPmtSpInfoMapperTest.class.getResourceAsStream("/keys/private-key.pfx");
@@ -37,14 +34,10 @@ public class GepgPmtSpInfoMapperTest {
             Files.copy(resourceStream, tempFile, StandardCopyOption.REPLACE_EXISTING);
 
             // Set the keystore path to the temporary file location
-            keystorePath = tempFile.toAbsolutePath().toString();
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("Failed to load keystore", e);
         }
-
-        keystorePassword = "passpass";
-        keyAlias = "gepgclient";
     }
 
     @Test
@@ -53,7 +46,7 @@ public class GepgPmtSpInfoMapperTest {
         GepgPmtSpInfo pmtSpInfoMapper = createData();
 
         // Convert to XML string
-        String message = XmlUtil.convertToXmlString(pmtSpInfoMapper);
+        String message = gepgApiClient.convertToXmlString(pmtSpInfoMapper);
 
         String expectedXml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><gepgPmtSpInfo><PymtTrxInf><TrxId>TRX123456</TrxId><SpCode>SP001</SpCode><PayRefId>PAYREF123456</PayRefId><BillId>74c7c4ee-b9d1-4a90-bb71-c999b7b6b09c</BillId><PayCtrNum>PAYCTR123456</PayCtrNum><BillAmt>1000.0</BillAmt><PaidAmt>1000.0</PaidAmt><BillPayOpt>1</BillPayOpt><CCy>TZS</CCy><TrxDtTm>2022-01-01T12:00:00</TrxDtTm><UsdPayChnl>MOBILE</UsdPayChnl><PyrCellNum>255712345678</PyrCellNum><PyrName>JohnDoe</PyrName><PyrEmail>johndoe@example.com</PyrEmail><PspReceiptNumber>PSPREC123456</PspReceiptNumber><PspName>PSPName</PspName><CtrAccNum>CTRACC123456</CtrAccNum></PymtTrxInf></gepgPmtSpInfo>";
 
@@ -66,7 +59,7 @@ public class GepgPmtSpInfoMapperTest {
         GepgPmtSpInfo pmtSpInfoMapper = createData();
 
         // Convert to XML string
-        String message = XmlUtil.convertToXmlStringWithoutDeclaration(pmtSpInfoMapper);
+        String message = gepgApiClient.convertToXmlStringWithoutDeclaration(pmtSpInfoMapper);
 
         String expectedXml = "<gepgPmtSpInfo><PymtTrxInf><TrxId>TRX123456</TrxId><SpCode>SP001</SpCode><PayRefId>PAYREF123456</PayRefId><BillId>74c7c4ee-b9d1-4a90-bb71-c999b7b6b09c</BillId><PayCtrNum>PAYCTR123456</PayCtrNum><BillAmt>1000.0</BillAmt><PaidAmt>1000.0</PaidAmt><BillPayOpt>1</BillPayOpt><CCy>TZS</CCy><TrxDtTm>2022-01-01T12:00:00</TrxDtTm><UsdPayChnl>MOBILE</UsdPayChnl><PyrCellNum>255712345678</PyrCellNum><PyrName>JohnDoe</PyrName><PyrEmail>johndoe@example.com</PyrEmail><PspReceiptNumber>PSPREC123456</PspReceiptNumber><PspName>PSPName</PspName><CtrAccNum>CTRACC123456</CtrAccNum></PymtTrxInf></gepgPmtSpInfo>";
 
@@ -75,17 +68,15 @@ public class GepgPmtSpInfoMapperTest {
 
     @Test
     public void testSignedMessageContainsGepgAndSignature() throws Exception {
+        GepgApiClient gepgApiClient = new GepgApiClient();
         // Create a sample message
         GepgPmtSpInfo pmtSpInfoMapper = createData();
 
         // Convert to XML string
-        String message = XmlUtil.convertToXmlStringWithoutDeclaration(pmtSpInfoMapper);
-
-        // Instantiate MessageUtil
-        MessageUtil messageUtil = new MessageUtil(keystorePath, keystorePassword, keyAlias);
+        String message = gepgApiClient.convertToXmlStringWithoutDeclaration(pmtSpInfoMapper);
 
         // Sign the message
-        String signedMessage = messageUtil.sign(message, GepgPmtSpInfo.class);
+        String signedMessage = gepgApiClient.signMessage(message, GepgPmtSpInfo.class);
 
         // Assert that the signed message is not null and contains <Gepg> and
         // <gepgSignature>
@@ -99,23 +90,23 @@ public class GepgPmtSpInfoMapperTest {
         // Creating and populating the Payment Transaction Information
 
         GepgPymtTrxInf pymtTrxInf = new GepgPymtTrxInf(
-            "TRX123456",                            // trxId
-            "SP001",                                // spCode
-            "PAYREF123456",                         // payRefId
-            UUID.fromString("74c7c4ee-b9d1-4a90-bb71-c999b7b6b09c"), // billId
-            "PAYCTR123456",                         // payCtrNum
-            1000.0,                                 // billAmt
-            1000.0,                                 // paidAmt
-            "1",                         // billPayOptString (corresponds to FULL payment option)
-            "TZS",                                  // CCy
-            LocalDateTime.parse("2022-01-01T12:00:00"), // trxDtTm
-            "MOBILE",                               // usdPayChn
-            "255712345678",                         // pyrCellNum
-            "JohnDoe",                              // pyrName
-            "johndoe@example.com",                  // pyrEmail
-            "PSPREC123456",                         // pspReceiptNumber
-            "PSPName",                              // pspName
-            "CTRACC123456"                          // ctrAccNum
+                "TRX123456", // trxId
+                "SP001", // spCode
+                "PAYREF123456", // payRefId
+                UUID.fromString("74c7c4ee-b9d1-4a90-bb71-c999b7b6b09c"), // billId
+                "PAYCTR123456", // payCtrNum
+                1000.0, // billAmt
+                1000.0, // paidAmt
+                "1", // billPayOptString (corresponds to FULL payment option)
+                "TZS", // CCy
+                LocalDateTime.parse("2022-01-01T12:00:00"), // trxDtTm
+                "MOBILE", // usdPayChn
+                "255712345678", // pyrCellNum
+                "JohnDoe", // pyrName
+                "johndoe@example.com", // pyrEmail
+                "PSPREC123456", // pspReceiptNumber
+                "PSPName", // pspName
+                "CTRACC123456" // ctrAccNum
         );
         // Creating and populating the Payment Service Provider Information
         GepgPmtSpInfo pmtSpInfo = new GepgPmtSpInfo(pymtTrxInf);
