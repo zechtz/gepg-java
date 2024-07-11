407cc187c1a2cb6ab1548eeebb0796d513b434cc
Zachariah Ngonyani <zachariahn@gmail.com>
2024-07-11 09:49:14 +0300

    feat: create an online payment info ack object

---
+++ gepg/src/main/java/com/watabelabs/gepg/mappers/payment/GepgOlPmtNtfSpInfoAck.java
@@ -0,0 +1,82 @@
+package com.watabelabs.gepg.mappers.payment;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * The GepgBillSubReqAckMapper class is used for mapping the XML response
+ * to the corresponding Java object. This class represents the acknowledgment
+ * of the bill subscription request.
+ *
+ * <p>
+ * It contains a single field, olStsCode, which stores the transaction status
+ * code of the acknowledgment response.
+ * </p>
+ *
+ * <p>
+ * The class is annotated with JAXB annotations to specify how the XML elements
+ * should be mapped to the Java fields. It uses {@link XmlRootElement} to define
+ * the
+ * root element name and {@link XmlAccessorType} to specify the access type for
+ * the fields.
+ * </p>
+ */
+@XmlRootElement(name = "gepgOlPmtNtfSpAck")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class GepgOlPmtNtfSpInfoAck {
+
+    @XmlElement(name = "OlStsCode")
+    private int olStsCode;
+
+    /**
+     * Default no-argument constructor.
+     */
+    public GepgOlPmtNtfSpInfoAck() {
+    }
+
+    /**
+     * Parameterized constructor to initialize the object with the provided
+     * transaction status code.
+     *
+     * @param olStsCode the transaction status code to set
+     */
+    public GepgOlPmtNtfSpInfoAck(int olStsCode) {
+        this.olStsCode = olStsCode;
+    }
+
+    /**
+     * Retrieves the transaction status code.
+     *
+     * @return the transaction status code
+     */
+    public int getOlStsCode() {
+        return olStsCode;
+    }
+
+    /**
+     * Sets the transaction status code.
+     *
+     * @param olStsCode the transaction status code to set
+     */
+    public void setOlStsCode(int olStsCode) {
+        this.olStsCode = olStsCode;
+    }
+
+    /**
+     * Returns a string representation of the object.
+     * <p>
+     * This method is overridden to provide a string representation of the object
+     * that includes the value of the olStsCode field.
+     * </p>
+     *
+     * @return a string representation of the object
+     */
+    @Override
+    public String toString() {
+        return "GepgOlPmtSpInfoAckMapper{" +
+                "olStsCode='" + olStsCode + '\'' +
+                '}';
+    }
+}
