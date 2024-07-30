bf0b117c714604a6deca7606918560fbd6468357
Zachariah Ngonyani <zachariahn@gmail.com>
2024-07-30 13:39:22 +0300

    feat: generate rabbitmq submission headers

---
+++ gepg/src/main/java/com/watabelabs/gepg/amqp/enums/GepgQueueHeaderRequestType.java
@@ -0,0 +1,5 @@
+package com.watabelabs.gepg.amqp.enums;
+
+public enum GepgQueueHeaderRequestType {
+    REQUEST_TYPE,
+}
---
+++ gepg/src/main/java/com/watabelabs/gepg/amqp/headers/QueueHeaders.java
@@ -0,0 +1,10 @@
+package com.watabelabs.gepg.amqp.headers;
+
+public class QueueHeaders {
+    public static final String BILL_SUBMISSION_HEADER = "BILL_SUBMISSION_REQUEST_HEADER";
+    public static final String CONTROL_NUMBER_REUSE_HEADER = "CONTROL_NUMBER_REUSE_REQUEST_HEADER";
+    public static final String BILL_UPDATE_HEADER = "BILL_UPDATE_REQUEST_HEADER";
+    public static final String BILL_CANCELLATION_HEADER = "BILL_CANCELLATION_REQUEST_HEADER ";
+    public static final String PAYMENT_SUBMISSION_HEADER = "PAYMENTS_REQUEST_HEADER ";
+    public static final String RECONCILIATION_SUBMISSION_HEADER = "RECONCILIATION_SUBMISSION_REQUEST_HEADER ";
+}
